f5ec6b9f03a2fe7612d71362d98ed0075a56643d
ani <anirudh_narayan111@yahoo.com>
2014-10-22 15:38:49 -0400

    A templatized C++ BFS solver and three example problems.

---
+++ clock.cpp
@@ -0,0 +1,73 @@
+/*
+ * clock.cpp
+ * This file is part of cppBFS
+ *
+ * Copyright (C) 2014 - ani
+ *
+ * cppBFS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * cppBFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cppBFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ 
+#include <cstdlib>
+#include <vector>
+#include "bfssolver.h"
+
+using namespace std;
+
+class Clock1 : public solver<int> {
+public:
+	Clock1 (int hrs, int a, int final_hrs) : solver(a), hrs(hrs), final_hrs(final_hrs) {}
+
+	vector<int> get_next_cfg(int a) {
+		vector<int> vector_config;
+		if (a<0) {
+			vector_config.push_back(hrs+a);
+			vector_config.push_back(hrs+a+1);
+		} else if (a==0) {
+			vector_config.push_back(hrs-1);
+			vector_config.push_back(1);
+		}
+		else {
+			vector_config.push_back((a-1)%hrs);
+			vector_config.push_back((a+1)%hrs);
+		}
+		return vector_config;
+	}
+
+	void print_cfg(int x) {
+		cout<<x<<"\t";
+	}
+
+	bool goal_cfg(int x) {
+		return x==final_hrs;
+	}
+
+private:
+	int hrs, final_hrs;
+};
+
+
+int main (int argc, char * args[]) {
+	if (argc!=4) {
+		cout<<"Usage: ./file <clock hrs> <current time> <required time>"<<endl;
+		exit(1);
+	}
+	int hrs=atoi(args[1]);
+	int curr=atoi(args[2]);
+	int req=atoi(args[3]);
+	Clock1 c(hrs, curr, req);
+	c.process();
+//	c.display();
+	return 0;
+}
---
+++ hanoi.cpp
@@ -0,0 +1,99 @@
+/*
+ * hanoi.cpp
+ * This file is part of cppBFS
+ *
+ * Copyright (C) 2014 - ani
+ *
+ * cppBFS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * cppBFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cppBFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <cstdlib>
+#include <vector>
+#include <iomanip>
+#include <exception>
+#include "bfssolver.h"
+
+using namespace std;
+
+class TowOfHanoi : public solver<vector<vector<int> > > {
+public:
+	TowOfHanoi (int numPegs, int numDisks, vector<vector<int> > a) : solver(a), numPegs(numPegs), numDisks(numDisks) {}
+
+	vector< vector < vector<int> > > get_next_cfg(vector<vector<int> > a) {
+		vector< vector< vector<int> > > vector_config;
+		for (int i=0; i<numPegs; ++i) {
+			for (int j=0; j<numPegs /*&& j!=i*/; ++j) {
+				vector<vector<int> > tmp=a;
+				if (tmp.at(i).empty())
+					continue;
+				if ((!tmp.at(j).empty() && tmp.at(i).back()<tmp.at(j).back()) || tmp.at(j).empty()) {
+					tmp.at(j).push_back(tmp.at(i).back());
+					tmp.at(i).pop_back();
+					vector_config.push_back(tmp);
+				}
+			}
+		}
+		return vector_config;
+	}
+
+	bool goal_cfg(vector<vector<int> > x) {
+		vector<vector<int> >  curr(numPegs, vector<int>(0));
+		vector<int> tmpVec;
+		for (unsigned int i=numDisks; i>=1; --i)
+			tmpVec.push_back(i);
+		curr.at(numPegs-1)=tmpVec;
+		if (curr==x)
+			return true;
+		return false;
+	}
+
+	void print_cfg(vector<vector<int> > x) {
+		for (unsigned int i=0; i<x.size(); ++i) {
+			cout<<i+1<<"-->";
+			for (unsigned int j=0; j<x[i].size(); ++j) {
+				cout<<setw(3)<<x[i][j];
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+
+
+private:
+	int numPegs, numDisks;
+};
+
+
+int main (int argc, char * args[]) {
+	if (argc!=3) {
+		cout<<"Usage: ./file <numPegs> <numDisks>"<<endl;
+		exit(1);
+	}
+
+	int numPegs=atoi(args[1]);
+	int numDisks=atoi(args[2]);
+
+	vector<vector<int> >  curr(numPegs, vector<int>(0));
+
+	vector<int> tmpVec;
+	for (unsigned int i=numDisks; i>=1; --i)
+		tmpVec.push_back(i);
+
+	curr.at(0)=tmpVec;
+
+	TowOfHanoi t(numPegs, numDisks, curr);
+	t.process();
+	return 0;
+}
